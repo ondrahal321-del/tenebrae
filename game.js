5fb34f8ed770fa50d6b861668e3ee687e905a4ff
ondrahal321-del <ondra.hal321@gmail.com>
2026-07-04 04:23:10 +0200

    Update game.js

--- game.js
+++ game.js
@@ -159,7 +159,7 @@ class Character {
 	input.style.visibility="hidden";
 
     // Display the win message
-    displayText.innerHTML = `You defeted main threat in the city and folllowers of Pure Worship has been scattered! You win! <button onclick="location.href='index.html'">Return to Menu</button>`;
+    displayText.innerHTML = `You defeted main threat in the city and folllowers of Pure Worship has been scattered! You win! <button id="buttonMenu" onclick="location.href='index.html'">Return to Menu</button>`;
   }
 
 takeDamage(amount) {
